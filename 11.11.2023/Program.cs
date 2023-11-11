59816ccd9cc0091b1baff9956ae4230834c8df1b
khammad5 <khammadakh@code.edu.az>
2023-11-11 10:09:32 +0400

    Add project files.

---
+++ 11.11.2023/Program.cs
@@ -0,0 +1,13 @@
+﻿using System.Text;
+#region task1
+string words = "Salamnecesen"; string ReversedOne(string value)
+{
+    StringBuilder builder = new StringBuilder();
+    for (int i = value.Length - 1; i >= 0; i--)
+    {
+        builder.Append(value[i]);
+    }
+    return builder.ToString();
+    
+}
+#endregion
